a92d6e3940deec5be645e6b2e8fbd25f12c20e18
Scott Puhl <muddlegeist@gmail.com>
2024-06-28 14:20:42 -0500

    build Example locally from fork
    build Example locally from fork

--- Example/MZDownloadManager/MZDownloadManagerViewController.swift
+++ Example/MZDownloadManager/MZDownloadManagerViewController.swift
@@ -7,7 +7,6 @@
 //
 
 import UIKit
-import MZDownloadManager
 
 let alertControllerViewTag: Int = 500
 
--- Example/Tests/Tests.swift
+++ Example/Tests/Tests.swift
@@ -1,6 +1,5 @@
 import UIKit
 import XCTest
-import MZDownloadManager
 
 class Tests: XCTestCase {
     
